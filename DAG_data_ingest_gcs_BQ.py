cfc74cf27c76916557f7f206b1b5bedb2b0a3f00
Parag <91626613+Paragkk@users.noreply.github.com>
2021-12-10 07:05:41 +0000

    Update DAG_data_ingest_gcs_BQ.py

--- DAG_data_ingest_gcs_BQ.py
+++ DAG_data_ingest_gcs_BQ.py
@@ -1,83 +1,92 @@
-
-"""
-DAG for Ingesting data
-"""
-import os
-import time
-from urllib.parse import urlparse
-
 from airflow import models
-from airflow.operators.bash import BashOperator
 from airflow.providers.google.cloud.operators.bigquery import (
     BigQueryCreateEmptyTableOperator,
     BigQueryCreateExternalTableOperator,
     BigQueryInsertJobOperator
 )
-from airflow.utils.dates import days_ago
+from airflow.providers.google.cloud.operators.gcs import GCSListObjectsOperator
 from airflow.providers.google.cloud.sensors.gcs import GCSObjectsWithPrefixExistenceSensor
 from airflow.providers.google.cloud.transfers.gcs_to_gcs import GCSToGCSOperator
-from airflow.providers.google.cloud.operators.gcs import (
-    GCSDeleteObjectsOperator,
-)
+from airflow.providers.google.cloud.operators.gcs import GCSDeleteObjectsOperator
 from airflow.utils.state import State
-from airflow.providers.google.cloud.operators.gcs import GCSListObjectsOperator
+from airflow.utils.dates import days_ago
+
 
 # Variables to be changed Start
-TABLE_NAME = "table_name"
-PATH_TO_UPLOAD_FILE_PREFIX =  "file_prefix"
+PROJECT_ID = "project_id"
+BQ_LOCATION = "us-east4"
+TABLE_NAME = "Service_Requests_from"
+DATA_FILE_PREFIX =  "Service Requests from"
 # Variables to be changed End
 
-PROJECT_ID = "project_id"
-BQ_LOCATION = "us-central1"
-DATASET_STAGE = "dataset_stage"
-DATASET_CURATED = "dataset_curated"
-DATASET_REPORT = "dataset_report"
-BUCKET_1 = "bucket_name"
-BUCKET_2 = "bucket_name"
-SCHEMA = f"schema_files/SCHEMA_{TABLE_NAME}.json"
+DATASET_STAGE = "demo_stage"
+DATASET_CURATED = "demo_curated"
+DATASET_REPORT = "demo_reports"
+BUCKET_1 = "pot-data-itops"
+BUCKET_2 = "pot-data-itops"
+SCHEMA_EXT = f"schema_files/SCHEMA_EXT_{TABLE_NAME}.json"
+SCHEMA_NATIVE = f"schema_files/SCHEMA_NATIVE_{TABLE_NAME}.json"
+QUERY_CURATED = f"SELECT *,REGEXP_EXTRACT(_FILE_NAME, r'/([^/]+)/?$') as file_name FROM `{PROJECT_ID}.{DATASET_STAGE}.{TABLE_NAME}_ext`"
+QUERY_REPORT = f"SELECT * FROM `{PROJECT_ID}.{DATASET_CURATED}.{TABLE_NAME}_curated`"
+INSERT_ROWS_QUERY_CURATED = (f"INSERT INTO {DATASET_CURATED}.{TABLE_NAME}_curated {QUERY_CURATED}")
+INSERT_ROWS_QUERY_REPORT = (f"INSERT INTO {DATASET_REPORT}.{TABLE_NAME}_report {QUERY_REPORT}" )
 
-INSERT_ROWS_QUERY_CURATED = (
-    f"INSERT {DATASET_CURATED}.{TABLE_NAME}_curated "
-    f"SELECT * FROM `{PROJECT_ID}.{DATASET_STAGE}.{TABLE_NAME}_ext` ;"
-)
-INSERT_ROWS_QUERY_REPORT = (
-    f"INSERT {DATASET_REPORT}.{TABLE_NAME}_report "
-    f"SELECT * FROM `{PROJECT_ID}.{DATASET_CURATED}.{TABLE_NAME}_curated` ;"
-)
 with models.DAG(
-    f"jj_ingest_{TABLE_NAME}",
+    f"Ingest_{TABLE_NAME}",
     schedule_interval=None,  # Override to match your needs
     start_date=days_ago(1),
-    tags=[f"tag_{TABLE_NAME}"],
+    tags=[f"batch_ingestion_pipeline"],
 ) as dag:
 
     # [START howto_sensor_object_with_prefix_exists_task]
     gcs_object_with_prefix_exists = GCSObjectsWithPrefixExistenceSensor(
         bucket=BUCKET_1,
-        prefix=PATH_TO_UPLOAD_FILE_PREFIX,
+        prefix=DATA_FILE_PREFIX,
         mode='poke',
         task_id="gcs_file_watcher",
     )
     # [END howto_sensor_object_with_prefix_exists_task]
     list_bucket_files = GCSListObjectsOperator(
-        task_id="list_bucket_files", bucket=BUCKET_1, prefix=PATH_TO_UPLOAD_FILE_PREFIX)
+        task_id="list_bucket_files", bucket=BUCKET_1, prefix=DATA_FILE_PREFIX)
+
+    archive_old_file = GCSToGCSOperator(
+        task_id="archive_old_file",
+        source_bucket=BUCKET_1,
+        source_object=f"stage/{DATA_FILE_PREFIX}*",
+        destination_bucket=BUCKET_2,
+        destination_object=f"archive/{DATA_FILE_PREFIX}",
+        move_object=True,
+        retries=0,
+    )
+
+    stage_file = GCSToGCSOperator(
+        task_id="stage_file",
+        source_bucket=BUCKET_1,
+        source_object=f"{DATA_FILE_PREFIX}*",
+        destination_bucket=BUCKET_2,
+        destination_object=f"stage/{DATA_FILE_PREFIX}",
+        trigger_rule="all_done",
+        move_object=False,
+    )
+    list_stage_files = GCSListObjectsOperator(
+        task_id="list_stage_files", bucket=BUCKET_1, prefix=f"stage/{DATA_FILE_PREFIX}")
 
     create_external_table = BigQueryCreateExternalTableOperator(
         task_id="create_external_table",
         bucket=BUCKET_1,
-        source_objects=list_bucket_files.output,
+        source_objects= list_stage_files.output,
         destination_project_dataset_table=f"{DATASET_STAGE}.{TABLE_NAME}_ext",
         skip_leading_rows=1,
         allow_quoted_newlines =True ,
         allow_jagged_rows=True,
-        schema_object=SCHEMA,
+        schema_object=SCHEMA_EXT,
     )
 
     create_curated_table = BigQueryCreateEmptyTableOperator(
         task_id="create_curated_table",
         dataset_id=DATASET_CURATED,
         table_id=f"{TABLE_NAME}_curated",
-        gcs_schema_object=f"gs://{BUCKET_1}/{SCHEMA}",
+        gcs_schema_object=f"gs://{BUCKET_1}/{SCHEMA_NATIVE}",
         location=BQ_LOCATION,
     )
 
@@ -95,7 +104,7 @@ with models.DAG(
         task_id="create_reporting_table",
         dataset_id=DATASET_REPORT,
         table_id=f"{TABLE_NAME}_report",
-        gcs_schema_object=f"gs://{BUCKET_1}/{SCHEMA}",
+        gcs_schema_object=f"gs://{BUCKET_1}/{SCHEMA_NATIVE}",
         location=BQ_LOCATION,
     )
 
@@ -109,19 +118,13 @@ with models.DAG(
         },
         location=BQ_LOCATION,
     )
-    archieve_file = GCSToGCSOperator(
-        task_id="archieve_file",
-        source_bucket=BUCKET_1,
-        source_object=f"{PATH_TO_UPLOAD_FILE_PREFIX}*",
-        destination_bucket=BUCKET_2,
-        destination_object=f"archieve/{PATH_TO_UPLOAD_FILE_PREFIX}",
-    )
+    
 
     delete_files = GCSDeleteObjectsOperator(
         task_id="delete_files", bucket_name=BUCKET_1, objects=list_bucket_files.output
     )
 
-gcs_object_with_prefix_exists >> list_bucket_files >> create_external_table >> create_curated_table >> execute_insert_curated >> create_reporting_table >> execute_insert_report >> archieve_file >> delete_files
+gcs_object_with_prefix_exists >> list_bucket_files >> archive_old_file >> stage_file >> list_stage_files >> create_external_table >> create_curated_table >> execute_insert_curated >> create_reporting_table >> execute_insert_report >> delete_files
 
 if __name__ == '__main__':
     dag.clear(dag_run_state=State.NONE)
